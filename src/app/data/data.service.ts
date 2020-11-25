60c54dce39342b44d6585ea5d1ca5a1784f08a02
Jirinka <pousteckajir@seznam.cz>
2020-11-25 15:56:46 +0100

    Create registration form without validation

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+  
+  postRegistrationForm(user: User) : Observable<any>{
+   //return of(user);
+   return this.http.post('http://localhost:3000/api/v1/auth/sign-in', user);
+  }
+}
+
