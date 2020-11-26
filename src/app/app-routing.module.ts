08c0fb3218f4131d9721ee2ad49ea1bfd8258bb8
Andrea Malinova <an.malinova@seznam.cz>
2020-11-26 11:58:14 +0100

    main feed path changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component';
 
-const routes: Routes = [];
+const base = 'api/v1/';
+
+const routes: Routes = [
+    { path: '', redirectTo: base +'feed', pathMatch: 'full' },
+    { path: base + 'feed', component: FeedComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
