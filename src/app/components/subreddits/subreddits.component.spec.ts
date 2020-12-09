34b7065f0104658cd5e82a069c0f898fee16f5ce
Andrea Malinova <an.malinova@seznam.cz>
2020-12-09 15:30:04 +0100

    added welcomeComponent for main page

--- src/app/components/subreddits/subreddits.component.spec.ts
+++ src/app/components/subreddits/subreddits.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SubredditsComponent } from './subreddits.component';
 
 describe('SubredditsComponent', () => {
-  let component: SubredditsComponent;
-  let fixture: ComponentFixture<SubredditsComponent>;
+    let component: SubredditsComponent;
+    let fixture: ComponentFixture<SubredditsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SubredditsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SubredditsComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SubredditsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SubredditsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/subreddits/subreddits.component.ts
+++ src/app/components/subreddits/subreddits.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-subreddits',
-  templateUrl: './subreddits.component.html',
-  styleUrls: ['./subreddits.component.scss']
+    selector: 'app-subreddits',
+    templateUrl: './subreddits.component.html',
+    styleUrls: ['./subreddits.component.scss'],
 })
 export class SubredditsComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
