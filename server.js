84b1c5b61f9c7ee49c3146119fb7d0420b35f4c8
Martin Fučík <mar.fucik@gmail.com>
2020-12-16 21:10:33 +0100

    Little correction

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ice-age-reddit-frontend'));
+app.use(express.static(__dirname + '/ice-age-reddit-frontend/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/ice-age-reddit-frontend/index.html'));});
