6bb56de61c0543c481f83033a4539f0a38e9f86c
Martin Fučík <mar.fucik@gmail.com>
2020-12-16 18:21:39 +0100

    Deleted residual file

--- src/app/components/subreddits/subreddits.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-subreddits',
-    templateUrl: './subreddits.component.html',
-    styleUrls: ['./subreddits.component.scss'],
-})
-export class SubredditsComponent {}
