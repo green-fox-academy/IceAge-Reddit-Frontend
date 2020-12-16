eefff9c916c28d66eb36c911a8a15ad600846c37
Martin Fučík <mar.fucik@gmail.com>
2020-12-16 21:11:30 +0100

    another one

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/ice-age-reddit-frontend/dist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/ice-age-reddit-frontend/index.html'));});
+'/ice-age-reddit-frontend/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
