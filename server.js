14add41ca6a7ba1f63ec4b15ea019a4049e3953f
Martin Fučík <mar.fucik@gmail.com>
2020-12-16 21:25:51 +0100

    change path in server.js to project name with capital

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/ice-age-reddit-frontend/dist/'));
+app.use(express.static(__dirname + '/dist/IceAge-Reddit-Frontend'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/ice-age-reddit-frontend/dist/index.html'));});
+'/dist/IceAge-Reddit-Frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
