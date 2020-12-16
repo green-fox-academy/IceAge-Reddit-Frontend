9b129a83442bac66d099c901ae700cdb256751d0
Martin Fučík <mar.fucik@gmail.com>
2020-12-16 21:18:29 +0100

    if this is it i will kill myself

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/ice-age-reddit-frontend/dist'));
+app.use(express.static(__dirname + '/ice-age-reddit-frontend/dist/'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/ice-age-reddit-frontend/dist/index.html'));});
