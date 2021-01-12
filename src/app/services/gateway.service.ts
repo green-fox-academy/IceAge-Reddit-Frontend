621d8a4938c0a4456556dbfefa41730277bf947f
Jirinka <pousteckajir@seznam.cz>
2021-01-12 20:03:16 +0100

    fix failing test

--- src/app/services/gateway.service.ts
+++ src/app/services/gateway.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subreddit } from 'src/types/subreddits';
-import { Post, PostResponse } from 'src/types/posts';
+import { Post } from 'src/types/posts';
 import { User } from 'src/types/user';
 import { Error } from 'src/types/error';
 import { Token } from 'src/types/token';
