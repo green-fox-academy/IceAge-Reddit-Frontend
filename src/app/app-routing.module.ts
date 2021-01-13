f68d8b7f23600019ae0b308bca7a67c5c7296957
Martin Fučík <mar.fucik@gmail.com>
2021-01-13 19:53:05 +0100

    Added rout land-page and redirect there after first visit of site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,11 @@ import { FeedComponent } from './components//authorized/feed/feed.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SubredditsComponent } from './components/authorized/subreddits/subreddits.component';
 import { AuthGuard } from './guards/auth.guard';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 const routes: Routes = [
+    { path: '', redirectTo: '/feed', pathMatch: 'full' },
+    { path: 'land-page', component: WelcomePageComponent },
     { path: 'registration', component: RegistrationFormComponent },
     { path: 'login', component: LoginFormComponent },
 
