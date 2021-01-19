251e5a74ed12fdd4e44b40069206baa599d7f178
Jirinka <pousteckajir@seznam.cz>
2021-01-19 01:00:47 +0100

    Delete useless code 2

--- src/app/components/authorized/feed/feed.component.ts
+++ src/app/components/authorized/feed/feed.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { GatewayService } from 'src/app/services/gateway.service';
+import { Component } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
-import { SubredditService } from 'src/app/services/subreddit.service';
 import { Post } from 'src/types/posts';
-import { Subreddit } from 'src/types/subreddits';
 
 @Component({
     selector: 'app-feed',
@@ -19,12 +16,4 @@ export class FeedComponent {
             console.log(posts);
         });
     }
-
-    /* constructor(private _gatewayservice: GatewayService) {
-           this._gatewayservice.getAllPosts().subscribe((posts) => {
-            this._posts = posts;
-            console.log(posts);
-            console.log('feed rendered');
-        }); 
-    }*/
 }
