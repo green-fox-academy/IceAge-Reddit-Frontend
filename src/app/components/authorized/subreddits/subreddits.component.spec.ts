552c21d1ff5eef3070e849c1e05740739b9c3b4c
Andrea Malinova <an.malinova@seznam.cz>
2021-02-01 13:10:45 +0100

    completed tests for subredditsComponent

--- src/app/components/authorized/subreddits/subreddits.component.spec.ts
+++ src/app/components/authorized/subreddits/subreddits.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { SubredditFormatPipe } from 'src/app/pipes/subreddit-format.pipe';
@@ -8,7 +9,6 @@ import { SubredditService } from 'src/app/services/subreddit.service';
 import { Post } from 'src/types/posts';
 import { Subreddit } from 'src/types/subreddits';
 import { SubredditComponent } from '../subreddit/subreddit.component';
-
 import { SubredditsComponent } from './subreddits.component';
 
 const testPosts: Post[] = [
@@ -90,12 +90,17 @@ class MockGatewayService {
     }
 }
 
+const mockRouter = {
+    navigate: jasmine.createSpy('navigate'),
+};
+
 fdescribe('SubredditsComponent', () => {
     let component: SubredditsComponent;
     let fixture: ComponentFixture<SubredditsComponent>;
     let postService: PostService;
     let subredditService: SubredditService;
     let gatewayService: GatewayService;
+    let router: Router;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -115,6 +120,7 @@ fdescribe('SubredditsComponent', () => {
             ],
             declarations: [SubredditsComponent, SubredditComponent, SubredditFormatPipe],
             providers: [
+                { provide: Router, useValue: mockRouter },
                 { provide: PostService, useClass: MockPostService },
                 { provide: SubredditService, useClass: MockSubredditService },
                 { provide: GatewayService, useClass: MockGatewayService },
@@ -122,51 +128,86 @@ fdescribe('SubredditsComponent', () => {
         }).compileComponents();
     });
 
+    function createComponent(): void {
+        fixture = TestBed.createComponent(SubredditsComponent);
+        component = fixture.componentInstance;
+    }
+
     beforeEach(() => {
         subredditService = TestBed.inject(SubredditService);
         postService = TestBed.inject(PostService);
         gatewayService = TestBed.inject(GatewayService);
+        router = TestBed.inject(Router);
     });
 
     it('should create', () => {
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(component).toBeTruthy();
     });
 
     it('should call gatewayService.getAllSubreddits in constructor', () => {
         let getAllSubredditsSpy = spyOn(gatewayService, 'getAllSubreddits').and.callThrough();
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(getAllSubredditsSpy).toHaveBeenCalled();
     });
 
     it('should call subredditService.setSubreddits with data retrieved from gatewayService.getAllSubreddits subscribtion in constructor', () => {
         spyOn(gatewayService, 'getAllSubreddits').and.returnValue(of(testSubreddits));
         let setSubredditsSpy = spyOn(subredditService, 'setSubreddits').and.callThrough();
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(setSubredditsSpy).toHaveBeenCalledWith(testSubreddits);
     });
 
     it('should call gatewayService.getAllPosts in constructor', () => {
         let getAllPostsSpy = spyOn(gatewayService, 'getAllPosts').and.callThrough();
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(getAllPostsSpy).toHaveBeenCalled();
     });
 
     it('should call postService.setPosts with data retrieved from gatewayService.getAllPosts subscribtion in constructor', () => {
         spyOn(gatewayService, 'getAllPosts').and.returnValue(of(testPosts));
         let setPostsSpy = spyOn(postService, 'setPosts').and.callThrough();
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(setPostsSpy).toHaveBeenCalledWith(testPosts);
     });
 
     it('component._subreddits should equal testSubreddits', () => {
-        fixture = TestBed.createComponent(SubredditsComponent);
-        component = fixture.componentInstance;
+        createComponent();
         expect(component._subreddits).toEqual(testSubreddits);
+    });
+
+    it('should test private method _navigateToSubredditComponent with parameter subredditName', () => {
+        const subredditName = 'Subreddit1';
+        createComponent();
+        component['_navigateToSubredditComponent'](subredditName);
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/auth/feed', subredditName]);
+    });
+
+    it('should test onSelect method with parameter "testSubreddit"', () => {
+        let testSubreddit = testSubreddits[0];
+        let currentPosts = [];
+        currentPosts.push(testPosts[0]);
+
+        let getSubredditPostsFeedByNameSpy = spyOn(gatewayService, 'getSubredditPostsFeedByName')
+            .and.returnValue(of(currentPosts))
+            .and.callThrough();
+
+        let setCurrentSubredditPostsSpy = spyOn(
+            postService,
+            'setCurrentSubredditPosts',
+        ).and.callThrough();
+
+        createComponent();
+
+        let navigateToSubredditComponentSpy = spyOn<any>(
+            component,
+            '_navigateToSubredditComponent',
+        ).and.callThrough();
+
+        component.onSelect(testSubreddit);
+
+        expect(getSubredditPostsFeedByNameSpy).toHaveBeenCalledWith(testSubreddit.name);
+        expect(setCurrentSubredditPostsSpy).toHaveBeenCalledWith(currentPosts);
+        expect(navigateToSubredditComponentSpy).toHaveBeenCalledWith(testSubreddit.name);
     });
 });
